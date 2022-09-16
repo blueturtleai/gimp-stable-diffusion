3e9f68c0a94c2388b811174e3698dccf82672b7e
blueturtleai <113246030+blueturtleai@users.noreply.github.com>
2022-09-16 15:56:39 +0200

    v1.0.1
    CHANGES
    - API
    Data is now transferred as JSON. It is checked, if the API version of the client and the server matches. If mismatch, user is requested to update plugin and/or server. 
    
    - Seed
    The seed of the generated image is now displayed in an additional layer. It can be passed to the server to generate the same image again. This only works, if the same parameters (init strength, number of steps, etc) are used.

--- gimp-stable-diffusion.py
+++ gimp-stable-diffusion.py
@@ -1,60 +1,85 @@
 #!/usr/bin/python
 
-# v1.0.0
+# v1.0.1
 
 import urllib2
 import tempfile
 import os
+import base64
+import json
 
 from gimpfu import *
 
 INIT_FILE = "init.png"
 GENERATED_FILE = "generated.png"
 API_ENDPOINT = "api/img2img"
+API_VERSION = 2
 
 initFile = r"{}".format(os.path.join(tempfile.gettempdir(), INIT_FILE))
 generatedFile = r"{}".format(os.path.join(tempfile.gettempdir(), GENERATED_FILE))
 
 def getImageData(image, drawable):
    pdb.file_png_save_defaults(image, drawable, initFile, initFile)
-   imageFile = open(initFile, "rb")
-   return imageFile.read()
+   initImage = open(initFile, "rb")
+   encoded = base64.b64encode(initImage.read())
+   return encoded
 
-def displayGenerated(image):
+def displayGenerated(seed):
    image = pdb.file_png_load(generatedFile, generatedFile)
    pdb.gimp_display_new(image)
+   # image, drawable, x, y, text, border, antialias, size, size_type, fontname
+   pdb.gimp_text_fontname(image, None, 2, 2, str(seed), -1, TRUE, 12, 1, "Sans")
+   pdb.gimp_image_set_active_layer(image, image.layers[1])
    return
 
-def img2img(image, drawable, initStrength, promptStrength, steps, prompt, url):
-   headers = {
+def img2img(image, drawable, initStrength, promptStrength, steps, prompt, seed, url):
+   data = {
       "init_strength": float(initStrength),
       "prompt_strength": float(promptStrength),
       "steps": int(steps),
       "width": image.width,
       "height": image.height,
-      "prompt": prompt
+      "prompt": prompt,
+      "seed": seed,
+      "api_version": API_VERSION
    }
 
-   headers.update({"Content-Type": "application/octet-stream"})
-
    imageData = getImageData(image, drawable)
+   data.update({"init_img": imageData})
+   data = json.dumps(data)
+
+   headers = {"Content-Type": "application/json"}
 
    pdb.gimp_progress_set_text("starting dreaming now...")
    url = url + API_ENDPOINT
-   request = urllib2.Request(url=url, data=imageData, headers=headers)
-   response = urllib2.urlopen(request)
+   request = urllib2.Request(url=url, data=data, headers=headers)
 
-   imageFile = open(generatedFile, "wb+")
-   imageFile.write(response.read())
-   imageFile.close()
+   try:
+      response = urllib2.urlopen(request)
 
-   displayGenerated(image)
+      data = response.read()
+      data = json.loads(data)
 
-   if os.path.exists(initFile):
-       os.remove(initFile)
+      images = data["images"]
+      imageData = base64.b64decode(images[0]["image"])
 
-   if os.path.exists(generatedFile):
-       os.remove(generatedFile)
+      imageFile = open(generatedFile, "wb+")
+      imageFile.write(imageData)
+      imageFile.close()
+
+      displayGenerated(images[0]["seed"])
+
+      if os.path.exists(initFile):
+         os.remove(initFile)
+
+      if os.path.exists(generatedFile):
+         os.remove(generatedFile)
+
+   except Exception as ex:
+      if hasattr(ex, "code") and ex.code == 405:
+         raise Exception("GIMP plugin and stable-diffusion server don't match. Please update the GIMP plugin. If the error still occurs, please reopen the colab notebook.")
+      else:
+         raise ex
 
    return
 
@@ -69,9 +94,10 @@ register(
    "*",
    [
       (PF_SLIDER, "initStrength", "Init Strength", 0.3, (0.1, 0.9, 0.1)),
-      (PF_SLIDER, "promptStrength", "Prompt Strength", 7.5, (0, 20, 0.1)),
-      (PF_SLIDER, "steps", "Steps", 50, (10, 500, 1)),
+      (PF_SLIDER, "promptStrength", "Prompt Strength", 7.5, (0, 20, 0.5)),
+      (PF_SLIDER, "steps", "Steps", 50, (10, 150, 1)),
       (PF_STRING, "prompt", "Prompt", ""),
+      (PF_INT, "seed", "Seed", -1),
       (PF_STRING, "url", "Backend root URL", "")
    ],
    [],
